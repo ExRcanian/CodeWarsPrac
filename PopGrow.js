c6fb92bf1651c6d6893d4c8909690ddb4d19c38c
Hong <hoang.bui49@gmail.com>
2023-12-20 20:24:27 -0800

    Completed Challenge

---
+++ PopGrow.js
@@ -0,0 +1,14 @@
+function nbYear(currentPop, percentIncrease, migrants, targetPop) {
+    let per = percentIncrease/100
+    let years = 0
+
+    do {
+        currentPop = Math.floor(currentPop + (currentPop * per) + migrants)
+        console.log(currentPop);
+        years++
+    } while (currentPop < targetPop) {
+        
+    return years
+    } 
+}
+
