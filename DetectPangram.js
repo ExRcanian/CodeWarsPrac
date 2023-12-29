76200c1d08225dc64ce824b92d519f4a9e3ad739
Hong <hoang.bui49@gmail.com>
2023-12-28 22:10:39 -0800

    Complte

---
+++ DetectPangram.js
@@ -0,0 +1,16 @@
+function isPangram(string){
+    let pangram = true
+    const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    for (let i = 0; i < alphabet.length; i++) {
+        if (string.includes(alphabet[i])) {
+            pangram = true
+        } else {
+            pangram = false
+        }
+    }
+    if (pangram == true) {
+        return true
+    } else {
+        return false
+    }
+}
