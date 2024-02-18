457b525871197068a0a82430a8835f4ba0c1880f
Hong <hoang.bui49@gmail.com>
2024-02-17 19:57:55 -0800

    complete

--- FindUnique.js
+++ FindUnique.js
@@ -8,6 +8,6 @@ function findUniq(arr) {
     } else if (arr[0] != arr[1] && arr[0] != arr[2]) {
         console.log(arr[0]); 
     } else {
-        console.log(arr[1]);
+        console.log(arr[1]); 
     }
 }
